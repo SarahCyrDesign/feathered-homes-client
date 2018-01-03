c29676b5d71624029c68e09e5e472b457e7bc5a4
github username <github email address>
2018-01-02 20:13:14 -0500

    duplicate logo and complete Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        <img style={{width: 900, height: 189}} src={'/images/logo.png'} alt={'/images/adoptable_bird.png'} />
+    </div>
+  );
+};
+
+export default Home;
