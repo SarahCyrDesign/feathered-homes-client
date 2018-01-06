d13f367c3cdfe93f872148ff13635af908e754b3
github username <github email address>
2018-01-05 20:57:06 -0500

    stylize falling feather animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
+    <img className="feather" src={'/images/feather.png'} alt='feather' />
         <img src={'/images/hero-pic.jpg'} alt={'/images/adoptable_bird.png'} />
     </div>
   );
